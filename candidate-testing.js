1d9135a1768933e51f6d55a435e2cf91042ef630
Luis Avecilla <avecillalu92@gmail.com>
2024-01-22 21:48:50 -0500

    Passed 10 test, Needs logic fixing

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,19 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  'Who was the first American woman in space? ', 
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "What is the minimum crew size for the ISS? " ]
+let correctAnswers = [
+  'Sally Ride',
+  'true',
+  '40',
+  'Trajectory',
+  "3"]
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -26,17 +36,19 @@ function askForName() {
   
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  let question = input.question("Who was the first American woman in space? ");
-     candidateAnswer = question;
+  for (let i = 0; i < questions.length; i++) {
+  questions = input.question(`${questions[i]} Enter your answer: `);
+     candidateAnswers.push(candidateAnswers);
+    }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === "Sally Ride"){
-    console.log("You are correct! Sally Ride was the first American woman in space in 1983!");
-  } else {
-    console.log("Sorry, that was not the correct answer, Try again!");
+  for (let i = 0; i < questions.length; i++){
+   /* candidateAnswers = candidateAnswers[i];
+    correctAnswers = correctAnswers[i]; */
+    console.log(`Question ${i + 1}: You answered - ${candidateAnswers}`)
   }
 
 
